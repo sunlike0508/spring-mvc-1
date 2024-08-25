f91128d25f06e2f655cf8b0cdbf247aadbd7692c
seonhoshin <sunlike0508@waug.com>
2024-08-25 16:31:12 +0900

    초기화

---
+++ src/test/java/com/study/springmvc1/SpringMvc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springmvc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
